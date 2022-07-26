361447ab2b60752806dd0b09feec75ad17e1642d
Sherreka <rekadarnb@gmail.com>
2022-07-26 15:24:22 -0400

    adding About Me section

---
+++ src/components/About.js
@@ -0,0 +1,20 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function About() {
+  return (
+    <Container>
+      <Row>
+        <Col>
+        <div className='mt-3'>
+            <h2 className='text-center'>About Me</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur, quam. Quaerat est, sint necessitatibus, esse saepe dignissimos quo aut numquam cum nulla quasi error! Molestias quo consequuntur quam possimus nam?Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut modi ratione facilis recusandae officiis. Nam eum dolores quisquam. Natus ducimus excepturi dolorum praesentium quibusdam distinctio culpa? Est tempora ratione dolor! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae, architecto corporis! Inventore, deleniti voluptate vitae vero exercitationem beatae, voluptatum quis odit asperiores tenetur maxime commodi delectus voluptatem, possimus sit labore!</p>
+        </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default About;
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import Intro from './Intro';
+import About from './About';
 
 export default function Portfolio () {
   return (
       <div>
           <Header />
           <Intro />
+          <About />
       </div>
   )
 };
