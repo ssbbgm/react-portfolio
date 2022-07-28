b348cf694d70d179bad884b8074821125a352488
Sherreka <rekadarnb@gmail.com>
2022-07-28 16:06:49 -0400

    centered form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,33 +8,34 @@ function BasicExample() {
   return (
     <Container>
         <h2 className='text-center mt-3' id='contact'>Contact</h2>
-        <Form className='mb-0 w-50'>
-        <Row className="mb-3">
-            <Form.Group as={Col} controlId="formGridEmail">
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            </Form.Group>
+        <Container className="d-flex justify-content-center">
+            <Form className='mb-0 w-50'>
+            <Row>
+                <Form.Group as={Col} controlId="formGridFirstName">
+                <Form.Label>First Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter first name" required/>
+                </Form.Group>
 
-            <Form.Group as={Col} controlId="formGridPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+                <Form.Group as={Col} controlId="formGridLastName">
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter Last Name" required/>
+                </Form.Group>
+            </Row>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" required/>
             </Form.Group>
-      </Row>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-            Submit
-        </Button>
-        </Form>
+            <Form.Group className="mb-3" controlId="formBasicInfo">
+                <Form.Label>Message</Form.Label>
+                <Form.Control as='textarea' rows={3} placeholder="Insert Your Message"  required/>
+            </Form.Group>
+            <Container className='d-flex justify-content-center'>
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+            </Container>
+            </Form>
+        </Container>
     </Container>
   );
 }
