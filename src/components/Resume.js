dec4d49e8bc342c3aa8fd1291f5214bcbb5fa2d7
Sherreka <rekadarnb@gmail.com>
2022-07-28 17:02:07 -0400

    added resume section

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,28 +3,41 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 import resume from '../images/resume.jpg';
+import html from '../images/html.png';
+import css from '../images/css.png';
+import javascript from '../images/javascript.png';
+import sql from '../images/sql.png';
+
 
 
 function Resume() {
   return (
-    <Container>
+    <Container className='mt-3'>
     <h2 className='text-center mt-3' id='about'>Resume</h2>
-      <Row className='me-auto'>
+      <Row className='me-auto mt-3'>
         <Col>
-            <h3>Click image for a copy of my resume! </h3>
+            <h3>Click image for a copy of my resume!</h3>
             <a href='https://drive.google.com/file/d/1IHTezjZatwZdYPpu41erNZ6eqANcyvor/view?usp=sharing' target='_blank' rel='noreferrer'><Image src={resume}  rounded/></a>
         </Col>
         <Col>
             <Container>
                 <Row>
-                    <Col></Col>
-                    <Col></Col>
+                    <Col>
+                        <Image src={html}/>
+                    </Col>
+                    <Col>
+                        <Image src={css} />
+                    </Col>
                 </Row>
             </Container>
-            <Container>
+            <Container className='mt-5'>
                 <Row>
-                    <Col></Col>
-                    <Col></Col>
+                    <Col>
+                        <Image src={javascript}/>
+                    </Col>
+                    <Col>
+                        <Image src={sql}/>
+                    </Col>
                 </Row>
             </Container>
         </Col>
