fe8601c87409e188ae14b90861feccf40636322e
Sherreka <rekadarnb@gmail.com>
2022-07-28 15:50:25 -0400

    adding form info

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,29 @@
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 function BasicExample() {
   return (
     <Container>
         <h2 className='text-center mt-3' id='contact'>Contact</h2>
-        <Form>
+        <Form className='mb-0 w-50'>
+        <Row className="mb-3">
+            <Form.Group as={Col} controlId="formGridEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+      </Row>
         <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
             <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-            </Form.Text>
         </Form.Group>
-
         <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
             <Form.Control type="password" placeholder="Password" />
